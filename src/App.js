da4d1a4875a6b492eed2ecf00ae717fe91d4f244
Wilton Calvo Barrios <wilton>
2021-10-22 11:09:58 -0500

    Se crea el formulario para añadir tareas y se crean estados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import TaskForm from "./components/TaskForm";
 
 function App() {
+  const [listTask, setListTask] = useState([]);
+
+  const newTask = (task) => {
+    setListTask([task, ...listTask]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TaskForm newTask={newTask} />
     </div>
   );
 }
---
+++ src/components/TaskForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+const TaskForm = (props) => {
+  const [inputText, setInputText] = useState("");
+  const [validation, setValidation] = useState(true);
+
+  const handleForm = (e) => {
+    setInputText(e.target.value);
+    console.log(inputText);
+  };
+
+  const submit = (e) => {
+    e.preventDefault();
+    if (inputText.trim() !== "") {
+      props.newTask(inputText);
+      setInputText("");
+      setValidation(true);
+    } else {
+      setValidation(false);
+    }
+  };
+
+  return (
+    <div>
+      <form className="form" onSubmit={submit}>
+        <label>Ingresar tarea: </label>
+        <input value={inputText} onChange={handleForm} />
+        <button>Añadir</button>
+      </form>
+      {!validation && <div className="validation">Debe añadir una tarea.</div>}
+    </div>
+  );
+};
+
+export default TaskForm;
